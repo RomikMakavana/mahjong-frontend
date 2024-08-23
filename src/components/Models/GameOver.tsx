8b14d011714e45d7627288d3e2c1ee9b3b2c93df
mr-rpchauhan <rpchauhan11111@gmail.com>
2024-08-23 15:55:26 +0530

    Working on game over

--- src/components/Models/GameOver.tsx
+++ src/components/Models/GameOver.tsx
@@ -1,6 +1,7 @@
 import MahjongModel from "../MahjongModel";
 import { ICONS } from "@/helpers/icons";
 import Image from "next/image";
+import PrimaryButton from "../PrimaryButton";
 
 interface userDetails {
     userName: string;
@@ -30,81 +31,64 @@ export default function GameOver() {
                     userName: "dakshjoshi66758",
                     result: -50
                 },
-                {
-                    userName: "dakshjoshi66758",
-                    result: -50
-                },
-                {
-                    userName: "dakshjoshi66758",
-                    result: -50
-                },
-                {
-                    userName: "dakshjoshi66758",
-                    result: -50
-                },
-                {
-                    userName: "dakshjoshi66758",
-                    result: -50
-                },
-                {
-                    userName: "dakshjoshi66758",
-                    result: -50
-                },
-                {
-                    userName: "dakshjoshi66758",
-                    result: -50
-                },
             ]
         }
     ]
     return (
-        <MahjongModel open={true} extraCss="xs:w-[363px] max-h-[80%] overflow-hidden">
-            <div className="flex flex-col">
-                <Image src={ICONS.MahjongTextIcon} alt="Image" className="mx-auto" />
+        <MahjongModel open={true} extraCss="xs:w-[363px] max-h-[90%] overflow-hidden">
+            <div className="flex flex-col h-full">
+                <Image src={ICONS.MahjongTextIcon} alt="Image" className="mx-auto mb-4" />
                 {
                     users.map((user, index) => {
                         return (
-                            <div key={index} className="mt-4 flex flex-col justify-between">
-                                <div className="border-b border-white border-opacity-10 h-full">
-                                    <div className="border border-brand-blue rounded-9 p-3">
-                                        <div className="flex justify-between items-center">
-                                            <div className="flex items-center">
-                                                <Image src={ICONS.ImgAvatar} alt="Avatar Image"
-                                                    className="rounded-full w-[55px] border-teal-blue border" />
-                                                <div className="ml-[14px]">
-                                                    <p className="text-white font-medium text-sm">{user.wonUserName}</p>
-                                                    <p className="underline cursor-pointer text-xs text-pale-lavender">View winning tiles</p>
+                            <div key={index} className="flex flex-col">
+                                <div>
+                                    <div className="border-b border-white border-opacity-10">
+                                        <div className="border border-brand-blue rounded-9 p-3">
+                                            <div className="flex justify-between items-center">
+                                                <div className="flex items-center">
+                                                    <Image src={ICONS.ImgAvatar} alt="Avatar Image"
+                                                        className="rounded-full w-[55px] border-teal-blue border" />
+                                                    <div className="ml-[14px]">
+                                                        <p className="text-white font-medium text-sm">{user.wonUserName}</p>
+                                                        <p className="underline cursor-pointer text-xs text-pale-lavender">View winning tiles</p>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div>
-                                                <p className="text-sunset-orange font-bold text-base">Won ${user.result}</p>
+                                                <div>
+                                                    <p className="text-sunset-orange font-bold text-base">Won ${user.result}</p>
+                                                </div>
                                             </div>
                                         </div>
+                                        <p className="max-xs:text-xs text-white text-center my-5 font-medium text-sm">
+                                            Lvl {user.level} | Min Level {user.minLevel} | Pt per tile {user.pointsPerTile}
+                                        </p>
                                     </div>
-                                    <p className="max-xs:text-xs text-white text-center my-5 font-medium text-sm">
-                                        Lvl {user.level} | Min Level {user.minLevel} | Pt per tile {user.pointsPerTile}
-                                    </p>
-                                </div>
-                                <div className="mt-4 flex-grow overflow-auto h-full">
-                                    {
-                                        user.userDetails.map((details, index) => {
-                                            return (
-                                                <div key={index} className="flex justify-between items-center mb-5 last:mb-0">
-                                                    <div className="flex items-center">
-                                                        <Image src={ICONS.ImgAvatar} alt="Avatar Image"
-                                                            className="rounded-full w-[55px] border-teal-blue border" />
-                                                        <div className="ml-[14px]">
-                                                            <p className="text-white font-medium text-sm">{user.wonUserName}</p>
-                                                            <p className="underline cursor-pointer text-xs text-pale-lavender">View tiles</p>
+                                    <div className="mt-4 border">
+                                        <div className="flex-grow overflow-auto h-full">
+                                            {
+                                                user.userDetails.map((details, index) => {
+                                                    return (
+                                                        <div key={index} className="flex justify-between items-center mb-5 last:mb-0">
+                                                            <div className="flex items-center">
+                                                                <Image src={ICONS.ImgAvatar} alt="Avatar Image"
+                                                                    className="rounded-full w-[55px] border-teal-blue border" />
+                                                                <div className="ml-[14px]">
+                                                                    <p className="text-white font-medium text-sm">{user.wonUserName}</p>
+                                                                    <p className="underline cursor-pointer text-xs text-pale-lavender">View tiles</p>
+                                                                </div>
+                                                            </div>
+                                                            <div>
+                                                                <p className={`font-bold text-base ${details.result < 0 ? "text-brand-red" : "text-sunset-orange"}`}>{details.result < 0 ? "Lost" : "Won"} ${Math.abs(user.result)}</p>
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                    <div>
-                                                        <p className={`font-bold text-base ${details.result < 0 ? "text-brand-red" : "text-sunset-orange"}`}>{details.result < 0 ? "Lost" : "Won"} ${Math.abs(user.result)}</p>
-                                                    </div>
-                                                </div>
-                                            )
-                                        })
-                                    }
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                        <div className="">
+                                            <PrimaryButton label="Play again" />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         )
