7533f93384c77d509847abc14534b544eeba0b34
Romik <makavanaromik1214@gmail.com>
2024-10-02 18:17:49 +0530

    load api base url from the .env file

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -1,7 +1,5 @@
 const config = {
-    // baseUrl: import.meta.env.VITE_BASE_URL ? import.meta.env.VITE_BASE_URL : 'http://localhost:3034/web/',
-    // baseUrl:'http://localhost:3034/web/',
-    baseUrl: 'http://localhost:3031/web/',
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL? process.env.NEXT_PUBLIC_API_BASE_URL : 'http://localhost:3031/web/',
     endPoints: {
         createRoom:() => `game`,
         profile:() => `profile`,
